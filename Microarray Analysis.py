5ec9a03a20e04cb6c8248357749a8aee29567ecd
Tiffany Pankuch <31459605+tjp77@users.noreply.github.com>
2019-04-13 22:30:10 -0400

    Created initial program file

---
+++ Microarray Analysis.py
@@ -0,0 +1,26 @@
+#!/user/bin/python
+
+
+# Part I, 2.
+def Preprocess()
+    
+    
+    return 0;
+
+
+# Part II, 5.
+def Process()
+    
+    
+    return 0;
+
+
+def main()
+    
+    # plh
+    
+    return 0;
+    
+
+
+main();
