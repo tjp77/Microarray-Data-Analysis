38d5cb250d1de839be4a5b2354a1dfaa82fb3701
Tiffany Pankuch <31459605+tjp77@users.noreply.github.com>
2019-04-14 01:17:15 -0400

    Minor template additions

--- Microarray Analysis.py
+++ Microarray Analysis.py
@@ -1,6 +1,35 @@
 #!/user/bin/python
 
 
+def readInData(prompt):
+    
+    data = "";
+    not_read = True
+    
+    while (not_read):
+        
+        try:
+            
+            file_name = input(prompt);
+            
+            with open(file_name, "r") as file:
+                for line in file:
+                    
+                    # strip to be safe due to proir input whitespace problems.     
+                    data += line.strip();
+        except:
+        
+            print("\nTrouble reading input file. Make sure name is correct and the file is in the right format.\n");
+            
+        else:
+            print("\nFile read successfully.\n");
+            not_read = False;
+            file.close();
+    
+    return data.strip();
+
+
+
 # Part I, 2.
 def Preprocess()
     
@@ -24,3 +53,11 @@ def main()
 
 
 main();
+
+
+# ======= Notes =======
+#
+# "A class discovery procedure automatically discovered the distinction between
+# acute myeloid leukemia (AML) and acute lymphoblastic leukemia (ALL)"
+#
+#
