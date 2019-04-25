72a62fe4f014e00b548541715647827c7625a15b
Tiffany Pankuch <31459605+tjp77@users.noreply.github.com>
2019-04-24 21:14:01 -0400

    Additions to KNN, main, and ToDo list

--- Microarray Analysis.py
+++ Microarray Analysis.py
@@ -1,3 +1,278 @@
+#!/user/bin/python
+import sys;
+import numpy as np
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+
+class Gene:
+    
+    def __init__(self, acc, all, aml):
+        
+        self.accessionId = acc;
+        self.allList = all;
+        self.amlList = aml;
+
+
+def readInData(prompt, fileName, threshold):
+    
+    # array of gene object, each of gene class, each one line/row of the file data. 
+    genes = [];
+    notRead = True
+    
+    while (notRead):
+        
+        try:
+            
+            print(prompt); 
+            
+            with open(fileName, "r") as file:
+                
+                firstLine = file.readline().split("\t"); 
+                
+                #for i in range(0, len(firstLine)):
+                 #   print (str(i) + firstLine[i] + "|");
+                
+                # Not sure will need these, but have to pass through them either way.  
+                line = file.readline(); 
+                line = file.readline(); 
+                
+                # For first pass of while loop. 
+                line = file.readline();
+                
+                # Go through remaining lines and get all things interested in.
+                while line:
+                    
+                    line = line.split("\t")
+                    
+                     # For all where first col, description contains "(endogenous control)", skip since supossed to eliminate them.
+                    if ("endogenous control" in line[0]):
+                        
+                        line = file.readline();
+                        continue;
+                        
+                    # second column is Accession ID.
+                    accession = line[1];
+                    all = [];
+                    aml = [];
+                    
+                    j = 2;
+                    while (j < len(line) - 1):
+                        
+                        if ( (int)(line[j]) < threshold ):
+                            line[j] = threshold; 
+                        
+                        if ("ALL" in firstLine[j]):
+                            
+                            # ex: [88, A] for line 7 of input data file
+                            all.append([(int)(line[j]), line[j+1]]);
+                            
+                        elif ("AML" in firstLine[j]):
+                            
+                            aml.append([(int)(line[j]), line[j+1]]);
+                        
+                        j += 2;
+                    
+                    genes.append(Gene(accession, all, aml)); #print(genes[-1].allList, "\n\n");
+                    line = file.readline(); 
+        except:
+            print("\nTrouble reading input file. Make sure name is correct and the file is in the right format.\n");
+            sys.exit();
+            
+        else:
+            print("\nFile read successfully.\n");
+            notRead = False;
+            file.close();
+    
+    return [genes, firstLine];
+
+
+# Save Training Data, Part I
+def SaveAffymetrics1(genes, types):
+    
+    fileName = "affymetrics1.txt";
+        
+    try:
+            
+        file = open(fileName, "w");
+            
+        expCount = len(genes[0].allList) + len(genes[0].amlList);
+        label1 = " \t";
+        label2 = " \t";
+        #print(genes[0].allList); 
+            
+        j = 2; 
+            
+        for i in range(0, expCount):
+            
+            label1 += "EXP" + str(i + 1) + "\t";
+            label2 += "(" + types[j]  + ")\t";
+            j += 2;
+            
+        file.write(label1 + "\n");
+        file.write(label2 + "\n");
+            
+        WriteGenes(genes, file);
+        
+    except:
+        
+        print("\nError writing to ", fileName, "file.\n");
+
+    else:
+        print("\nFirst affymetrics save successful.\n");
+    
+    file.close();
+    return 0;
+
+
+# Save Testing Data, Part II 4.b
+def SaveAffymetrics3(genes, types):
+    
+    fileName = "affymetrics2.txt";
+        
+    try:
+        
+        file = open(fileName, "w");
+        
+        # ----==== Change label second here to however needed. ====----
+            
+        #expCount = len(genes[0].allList) + len(genes[0].amlList);
+        #label = " \t";
+            
+        #for i in range(0, expCount):
+                
+            #label += "Test_Exp" + str(i + 1) + "\t";
+            
+        #file.write(label + "\n");
+            
+        WriteGenes(genes, file);
+        
+    except:
+        
+        print("\nError writing to ", fileName, "file.\n");
+
+    else:
+        print("\nSecond affymetrics save successful.\n");
+    
+    file.close();
+    
+    return 0; 
+
+   
+# Save Testing Data, Part II 5 
+def SaveAffymetrics3(genes, types):
+    
+    fileName = "affymetrics3.txt";
+        
+    try:
+        
+        file = open(fileName, "w");
+            
+        expCount = len(genes[0].allList) + len(genes[0].amlList);
+        label = " \t";
+            
+        for i in range(0, expCount):
+                
+            label += "Test_Exp" + str(i + 1) + "\t";
+            
+        file.write(label + "\n");
+            
+        WriteGenes(genes, file);
+        
+    except:
+        
+        print("\nError writing to ", fileName, "file.\n");
+
+    else:
+        print("\nThird affymetrics save successful.\n");
+            
+    file.close();
+    return 0;
+
+
+
+def WriteGenes(genes, file):
+    
+    for i in range(0, len(genes)):
+                
+        file.write(genes[i].accessionId);
+                
+        for k in range(len(genes[i].allList)):
+                    
+            file.write("\t" + str(genes[i].allList[k][0]));
+                
+        for k in range(len(genes[i].amlList)):
+                    
+            file.write("\t" + str(genes[i].amlList[k][0]));
+                
+        file.write("\n");
+    
+    return 0;
+
+
+
+# Part I, 2.
+def Preprocess(genes):
+    
+    # Eliminate the genes with all As across the experiments, and replace all the expression values below some 
+    # threshold cut-off value to that threshold value (pick 20 to be the threshold cut-off value).
+    geneCount = len(genes);
+    i = 0;
+    
+    # All genes have the same amount so just set here. 
+    allCount = len(genes[0].allList);
+    amlCount = len(genes[0].amlList);
+    
+    while (i < 60): #--------------------------------------------- Set back to geneCount, lowered for faster testing purposes. 
+        
+        aCount = 0;
+        
+        for j in range(0, allCount):
+            
+            if ("A" in genes[i].allList[j][1]):
+                ++aCount;
+            
+        
+        for j in range(0, amlCount):
+            
+            if ("A" in genes[i].amlList[j][1]):
+                ++aCount;
+        
+        # [min, max]
+        minMax = GetMinMax(genes[i].allList + genes[i].amlList);
+        
+        # Eliminate genes with either all A tags, and eliminate the genes with less than two
+        # fold change across the experiments, where: max(exp1...exp38)/min(exp1...exp38) < 2. 
+        if ( (aCount == allCount + amlCount) or (minMax[1]/minMax[0] < 2) ):
+            del genes[i]; 
+        else:
+            i += 1; 
+    
+    return 0;
+
+
+def GetMinMax(list): 
+    
+    max = 0;
+    min = list[0][0];
+    
+    for i in range(0, len(list)):
+        if (list[i][0] > max ):
+            max = list[i][0];
+
+        if (list[i][0] < min):
+            min = list[i][0];
+    
+    return [min, max];
+
+
+
+# Part II, 4.
+def ProcessTraining(genes):
+    
+    
+    return 0;
+
+
 # Part III, KNN
 # https://scikit-learn.org/stable/modules/generated/sklearn.svm.libsvm.fit.html#sklearn.svm.libsvm.fit
 def ClassifyGenes(genesTraining, labelsTraining, genesTesting, labelsTesting):
